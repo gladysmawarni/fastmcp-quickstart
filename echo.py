5ddd497e69fbd6401ec554b962aeafa6d833b492
gladysmawarni <78975611+gladysmawarni@users.noreply.github.com>
2025-09-29 11:11:22 +0100

    Update echo.py

--- echo.py
+++ echo.py
@@ -1,30 +1,39 @@
-"""
-FastMCP Echo Server
-"""
-
 from fastmcp import FastMCP
+from firebase_admin import credentials, firestore, initialize_app
+import os, json
 
-# Create server
-mcp = FastMCP("Echo Server")
+from dotenv import load_dotenv
+load_dotenv()
 
+# credentials
+json_creds = json.loads(os.environ["FIREBASE_CRED"])
 
-@mcp.tool
-def echo_tool(text: str) -> str:
-    """Echo the input text"""
-    return text
-
-
-@mcp.resource("echo://static")
-def echo_resource() -> str:
-    return "Echo!"
-
-
-@mcp.resource("echo://{text}")
-def echo_template(text: str) -> str:
-    """Echo the input text"""
-    return f"Echo: {text}"
+# --- Firebase Setup ---
+cred = credentials.Certificate(json_creds)
+initialize_app(cred)
+db = firestore.client()
 
 
-@mcp.prompt("echo")
-def echo_prompt(text: str) -> str:
-    return text
+
+# Instantiate the server, giving it a name
+mcp = FastMCP(name="Firebase DB connection")
+
+@mcp.tool()
+def get_restaurant_info(name: str):
+    """Fetch restaurant info by name from Firestore."""
+    doc_ref = db.collection("restaurants").document(name)
+    doc = doc_ref.get()
+
+    # if restaurant exist
+    if doc.exists:
+      data = doc.to_dict()
+      return {"Restaurant name": data['GoogleData']['restaurant'],
+              "Restaurant address": data['Address'],
+              "Restaurant website": data['Website'],
+              "Restaurant rating": data['GoogleData']['google_rating']}
+              
+    # if it doesn't exist
+    else:
+      return {"error": f"Restaurant '{name}' not found"}
+    
+
